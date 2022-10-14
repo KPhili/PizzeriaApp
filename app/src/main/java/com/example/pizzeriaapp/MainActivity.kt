2a7dbfb48bb37d4032bba8adf143949dd26bb4c2
Fil <flip.04@mail.ru>
2022-10-14 11:27:09 +0300

    init

---
+++ app/src/main/java/com/example/pizzeriaapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pizzeriaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
