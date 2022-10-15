cbe164d2c7c7c5c8142202c86297466c550e3f14
Fil <flip.04@mail.ru>
2022-10-15 23:32:32 +0300

    recycler view divider

--- app/src/main/java/com/example/pizzeriaapp/presentation/views/MainFragment.kt
+++ app/src/main/java/com/example/pizzeriaapp/presentation/views/MainFragment.kt
@@ -4,28 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.example.pizzeriaapp.R
 import com.example.pizzeriaapp.databinding.FragmentMainBinding
 import com.example.pizzeriaapp.presentation.adapters.ProductsAdapter
 import com.example.pizzeriaapp.presentation.viewmodels.MainViewModel
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
-    private val viewModel:MainViewModel by inject()
+    private val viewModel: MainViewModel by inject()
     private val adapter: ProductsAdapter by inject()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
@@ -33,13 +33,24 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.productsView.adapter = adapter
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() = with(binding) {
+        productsView.adapter = adapter
         viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED){
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.product.collectLatest {
                     adapter.submitData(it)
                 }
             }
+        }
+        ContextCompat.getDrawable(requireContext(), R.drawable.rv_divider)?.let {
+            val dividerItemDecorator =
+                DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL).apply {
+                    setDrawable(it)
+                }
+            productsView.addItemDecoration(dividerItemDecorator)
         }
     }
 }
